5c6aa0f43032d7deff8f34eae6570ffca17a12de
Luiz Claudio Galvão de Medeiros <130595535+gallvones@users.noreply.github.com>
2024-03-12 22:00:17 -0300

    Add files via upload

---
+++ questão1.js
@@ -0,0 +1,15 @@
+/* Utilizei o node.js para imprimir o resultado final (soma) */
+
+var indice = 13;
+var soma = 0;
+var k = 0;
+
+if (k < indice){
+k = k + 1;
+soma = soma + k; 
+
+}
+console.log(soma);
+
+
+
---
+++ questão5.js
@@ -0,0 +1,15 @@
+var a = 12
+var b = 15
+
+console.log(a);
+
+var a = 15
+var b = 12
+
+console.log(a);
+
+/* Para não utilizar funções prontas ou novas tags, como solicitado, 
+decidi fazer dessa maneira. O JS é uma linguagem linear, o código é 
+executado pela máquina em ordem, neste caso do topo ao fim (vscode), então 
+esse tipo de resolução da certo, mas não é recomendado.
+*/
